90bd508fc0fb727f0b6f86abc68ec109a2e57380
ade maulani bilgis <iiigis0422@gmail.com>
2021-11-10 16:33:47 +0700

    menambahkan labpy2

---
+++ labpy2.py
@@ -0,0 +1,9 @@
+a=input("masukan data ke-1: ")
+b=input("masukan data ke-2: ")
+c=input("masukan data ke-3: ")
+if a>b>c:
+    print("yang terbesar adalah",a)
+if b>a>c:
+    print("yang terbesar adalah",b)
+else :
+    print("yang terbesar adalah",c)
